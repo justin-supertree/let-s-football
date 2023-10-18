fc15f8db2855d81f50a943c75fbba0ea1d7a8a5c
justin-supertree <justin1688@supertree.co>
2023-10-18 12:36:11 +0900

    feat: env 환경변수 세팅 및 풋터 적용

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,12 +3,32 @@ import React from 'react';
 
 const Container = styled.div`
   width: 100%;
-  height: 150px;
   background-color: #0b0b0b;
 `;
 
+const OptionBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 5rem;
+  color: white;
+
+  & > li {
+    list-style-type: none;
+  }
+`;
+
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <OptionBlock>
+        <li>About</li>
+        <li>History</li>
+        <li>Contact</li>
+        <li>Email</li>
+      </OptionBlock>
+    </Container>
+  );
 };
 
 export default Footer;
