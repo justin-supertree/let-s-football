80bb3c8cb7a36e20489113ff1465deabeda5e2c2
justin-supertree <justin1688@supertree.co>
2023-10-19 11:30:32 +0900

    feat: local login setting

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -157,16 +157,7 @@ const LoginModal = ({ isOpen, handleModalClose }: Props) => {
               <span>{text.kor.kakao}</span>
             </SocialLoginButton>
           </a>
-          {/* <a
-            href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${redirectUrl}callback/naver`}
-          >
-            <SocialLoginButton>
-              <figure>
-                <IconNaver />
-              </figure>
-              <span>{text.kor.naver}</span>
-            </SocialLoginButton>
-          </a> */}
+
           <a
             href={`https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&include_granted_scopes=true&response_type=code&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_URL}/callback/google`}
           >
