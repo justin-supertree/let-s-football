4e8e1749995872e64c0061376c2fb0ee2199c815
justin-supertree <justin1688@supertree.co>
2023-10-19 15:29:14 +0900

    fix: url fix 2

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -153,7 +153,7 @@ const LoginModal = ({ isOpen, handleModalClose }: Props) => {
         <Desc>{text.kor.desc}</Desc>
         <SocialLoginButtonBlock>
           <a
-            href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}redirect_uri=${process.env.NEXT_PUBLIC_URL}/callback/kakao&&response_type=code`}
+            href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_URL}/callback/kakao&&response_type=code`}
           >
             <SocialLoginButton>
               <figure>
